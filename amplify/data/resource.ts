59038c4994c49f85efbff3cee1d05a25fa0e551a
Pascal Martin <connect@pascalmartin.dev>
2025-04-19 14:09:45 +1000

    Added Button

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -18,5 +18,9 @@ export const data = defineData({
   schema,
   authorizationModes: {
     defaultAuthorizationMode: "apiKey",
+    // API Key is used for a.allow.public() rules
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    },
   },
 });
--- src/App.tsx
+++ src/App.tsx
@@ -5,42 +5,17 @@ import { Amplify } from "aws-amplify";
 
 const client = generateClient<Schema>();
 
+async function sayHello() {
+  const result = await client.queries.sayHello({
+    name: "Pascal",
+  });
+  console.log(result);
+}
+
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    console.log("Amplify.configure:");
-    console.log(Amplify.getConfig());
-  }, []);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
-  return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
-  );
+  <>
+    <button onClick={sayHello}>Click Me</button>
+  </>;
 }
 
 export default App;
