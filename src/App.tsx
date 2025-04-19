03af2366291a62301d10c162e98ace2c73c8a7e2
Pascal Martin <connect@pascalmartin.dev>
2025-04-19 15:04:24 +1000

    Started Authorisation project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
+/* THIS IS EXAMPLE CODE FOR TRIGGERING A LAMBDA FUNCTION IN THE BACKEND INCLUDING THE PACKAGES TO IMPORT
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-
 const client = generateClient<Schema>();
 
 async function sayHello() {
@@ -9,11 +9,20 @@ async function sayHello() {
   });
   console.log(result);
 }
+--> <button onClick={sayHello}>Click Me</button>   <-- This is button that would be in app component to trigger the function!
+*/
+
+import "./App.css";
+import { auth } from "./Auth1";
+import { Amplify } from "aws-amplify";
+import outputs from "../amplify_outputs.json";
+
+Amplify.configure(outputs);
 
 function App() {
   return (
     <>
-      <button onClick={sayHello}>Click Me</button>
+      <Auth></Auth>
     </>
   );
 }
