387ef74160ee986dd5967d09a628b24f73206c2b
Pascal Martin <connect@pascalmartin.dev>
2025-04-19 15:44:38 +1000

    Add sign up attributes

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ async function sayHello() {
 */
 
 import "./App.css";
-import { Auth } from "./Auth1";
+import { Auth } from "./Auth2";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
---
+++ src/Auth2.tsx
@@ -0,0 +1,17 @@
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+
+export function Auth() {
+  return (
+    <div>
+      <Authenticator signUpAttributes={["nickname"]}>
+        {({ signOut, user }) => (
+          <main>
+            <h1>Hello {user?.username}</h1>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        )}
+      </Authenticator>
+    </div>
+  );
+}
